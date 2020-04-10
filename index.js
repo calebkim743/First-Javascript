4c8ea0ec159c8c987858119d7236010db779f7df
Caleb Kim <calebkim743@gmail.com>
2020-04-10 17:10:39 +0900

    day2

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 let gameCharacter = {
   name: 'Caleb',
-  class:'Human'
-  get title () {
-      return this.name+'the'+this.class;
-  }
+  class: 'Human',
+  health: 100,
+  get title() {
+    return this.name + 'the' + this.class;
+  },
+  set maxhealth(h) {
+    this.health = h;
+  },
   xpos: 0,
   items: ['knife', 'pork meat'],
   move: function (x) {
     this.xpos += x;
   },
 };
-gameCharacter.title;
-
